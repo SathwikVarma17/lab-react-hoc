69d848d81b2a71ab822a1a1849fb04fb523729b9
sathwik <sathwikvarma17@gmail.com>
2024-04-22 11:55:22 +0530

    updated

---
+++ hoc/src/components/LikePost.jsx
@@ -0,0 +1,14 @@
+
+import HigherorderCounter from './counter'
+import '../App.css'
+ function LikePost(props) {
+  const count=props.count;
+  const increment=props.increment
+  return (
+    <div>
+      <button onClick={increment}>Like Post {count}</button>
+    </div>
+  )
+}
+
+export default HigherorderCounter(LikePost)
